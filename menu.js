ef241ed042f99d5730e9deb8e2f0e5eb0eeeb88c
mert.surucuoglu <mertsurucuogluu@gmail.com>
2018-12-06 14:29:16 +0300

    mobile scale test

--- menu.js
+++ menu.js
@@ -10,6 +10,8 @@ Menu.preload = function () {
     //------------scale the game-----------
     game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
     game.scale.setMinMax(game.width / 3, game.height / 3, game.width, game.height);
+    game.scale.pageAlignHorizontally = false;game.scale.pageAlignVertically = true;
+
     game.scale.compatibility.forceMinimumDocumentHeight = true;
     game.scale.refresh();
 };
